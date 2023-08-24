e1c288f5ec4109b85b76ba7bf7a077c55ee219e3
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-08-24 18:12:30 +0530

    basic of routes

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,27 @@
-console.log("hello world");
+//  import * as express from 'express';
+ import express, { Request, Response, NextFunction } from 'express';
+
+ const app:express.Application=express();
+ app.listen(5000,()=>{
+    console.log("server is starting at port 5000");
+
+ })
+
+
+ const user={firstname:'abc',lastname:'xyz'};
+
+//  app.get('/login',(req,res)=>{
+//     res.status(200).send({message:"success"});
+//  });
+
+app.get('/login',(req,res)=>{
+    const data = [{name:'Ajay Verma'}];
+    res.send(data);
+  });
+
+
+
+ app.post('/signup');//when somthing want to insert resources
+ app.patch('/update/password');//patch-->only some detail of user will update 
+ app.put('/');//put-->all detail of user will replace
+ app.delete('');//when user to delete 
